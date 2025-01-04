529de5a96a7a2b0e55d59c276e97d3916ba4758e
imranhossen21 <imranhossen0586@gmail.com>
2025-01-05 03:04:14 +0600

    4th MD done

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express()
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 // middleware 
@@ -43,6 +43,19 @@ async function run() {
         app.post('/carts', async (req, res) => {
             const cartInfo = req.body;
             const result = await cartsCollection.insertOne(cartInfo)
+            res.send(result)
+        })
+
+        app.get('/carts', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email }
+            const result = await cartsCollection.find(query).toArray();
+            res.send(result)
+        });
+        app.delete('/carts/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await cartsCollection.deleteOne(query);
             res.send(result)
         })
 
